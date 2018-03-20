83863aae1f5f18b9ff445de982afd75da87a06fe
Rohit Padma <rohitp32@gmail.com>
2018-03-20 11:39:23 -0400

    Added View Actions methods in test class

--- app/src/androidTest/java/f1/com/example/rohit/homework2/MainActivityTest.java
+++ app/src/androidTest/java/f1/com/example/rohit/homework2/MainActivityTest.java
@@ -5,6 +5,7 @@ import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.core.deps.guava.base.Predicate;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -13,9 +14,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.core.IsInstanceOf;
 import org.junit.Before;
@@ -29,6 +32,7 @@ import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.core.deps.guava.base.Predicates.instanceOf;
@@ -48,6 +52,7 @@ public class MainActivityTest {
     private String moviedescription;
     private  String movieyear;
     private String movieimdb;
+    public static final String INVALID_COUNTRY_NAME = "Family";
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
@@ -59,32 +64,80 @@ public class MainActivityTest {
         moviename = "moviename";
         moviedescription="movie description";
         movieyear="2017";
-        movieimdb="5.6";
+        movieimdb="https://imdb.com/moviename";
 
     }
 
     @Test
     public void mainActivityTest() {
 
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.btnadd), withText("Add Movie"),
-                        isDisplayed()));
-        appCompatButton.perform(click());
+        onView(allOf(withId(R.id.btnadd), withText("Add Movie"),
+                        isDisplayed())).perform(click());
 
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.edtxtname),
+        onView(allOf(withId(R.id.edtxtname),
 
-                        isDisplayed()));
-
-        appCompatEditText.perform(replaceText("m"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText2 = onView(
+                        isDisplayed())).perform(replaceText(moviename), closeSoftKeyboard());
+        onView(
                 allOf(withId(R.id.edtxdesc),
+                        isDisplayed())).perform(replaceText(moviedescription),closeSoftKeyboard());
 
-                        isDisplayed()));
-        appCompatEditText2.perform(click());
+        onView(allOf(withId(R.id.edtxtimdblink),isDisplayed())).perform(replaceText(movieimdb),closeSoftKeyboard());
+
+        onView(allOf(withId(R.id.edtxtyear),isDisplayed())).perform(replaceText(movieyear),closeSoftKeyboard());
+
+        onView(allOf(withId(R.id.Sbrating),isDisplayed())).perform(setProgress(2));
+        onView(allOf(withId(R.id.spgenre),isDisplayed())).perform(setPosition(2));
+
+
+        onView(allOf(withId(R.id.btnaddmovie),
+                isDisplayed())).perform(click());
+
+        onView(allOf(withId(R.id.btnedit),
+                isDisplayed())).perform(click());
+
+
+    }
+
+    public static ViewAction setPosition(final int pos)
+    {
+        return new ViewAction() {
+            @Override
+            public void perform(UiController uiController, View view) {
+                Spinner sb = (Spinner) view;
+                 sb.setSelection(pos);
+            }
+
+            @Override
+            public String getDescription() {
+                return "Set a progress";
+            }
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(Spinner.class);
+            }
+        };
     }
 
 
 
+    public static ViewAction setProgress(final int progress) {
+        return new ViewAction() {
+            @Override
+            public void perform(UiController uiController, View view) {
+                SeekBar seekBar = (SeekBar) view;
+                seekBar.setProgress(progress);
+            }
+
+            @Override
+            public String getDescription() {
+                return "Set a progress";
+            }
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(SeekBar.class);
+            }
+        };
+    }
 }
